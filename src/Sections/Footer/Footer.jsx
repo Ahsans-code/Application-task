41ee0a0f75847782658d549c5a1dfede83ce7716
Ahsan Ali <ahsan326ali@gmail.com>
2025-10-31 11:10:33 +0500

    Fix: responsiveness footer

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <footer className="bg-[#2d2f3b]">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-[#a9aeb8]">
                 {/* Main footer content */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-16 max-lg:gap-y-4">
 
                     {/* Column 1: Logo & Contact */}
                     <div className="space-y-6">
